37542ae7b3af269037f4945ddf6024174b21baeb
Umer-Ali7 <ua472443@gmail.com>
2026-08-11 15:42:46 +0500

    Optimize images and reel loading

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+    qualities: [80],
+    deviceSizes: [320, 375, 430, 640, 768, 1024, 1280, 1440, 1920],
+  },
 };
 
 export default nextConfig;
